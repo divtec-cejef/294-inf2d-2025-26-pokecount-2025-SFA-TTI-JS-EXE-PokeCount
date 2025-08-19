ab774492270aa1ab7117521b27418b4f4383c9a6
Ethan Froidevaux <ethan.froidevaux@divtec.ch>
2025-08-19 15:53:27 +0200

    Ajout script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+/**
+ * Fichier JavaScript pour l'application PokeCount.
+ * @author Ethan Froidevaux <ethan.froidevaux@divtec.ch>
+ * @version 0.1 (Version actuelle)
+ * @since 2025-08-19 (Date de création)
+ */
